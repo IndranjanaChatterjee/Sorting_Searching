50f81f32187b3e6ed48126b03090fcef730e822c
Indranjana <112919974+IndranjanaChatterjee@users.noreply.github.com>
2023-12-11 14:16:14 +0530

    Update Selection_Sort.c

--- C/Selection_Sort.c
+++ C/Selection_Sort.c
@@ -1,1 +1,46 @@
-
+#include<stdio.h>
+void print(int a[],int len)
+{
+	int i;
+	for(i=0;i<len;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	
+}
+int main()
+{
+	int n,t;
+	printf("\nEnter the size of the array:");
+	scanf("%d",&n);
+	int a[n];
+	printf("\nEnter the elements:");
+	int i,j;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	print(a,n);
+	int min=a[0];
+	for(i=0;i<(n-1);i++)
+	{
+		min=i;
+		for(j=i+1;j<n;j++)
+		{
+			if(a[j]<a[min])
+			{
+				min=j;
+			}
+		}
+		t=a[i];
+		a[i]=a[min];
+		a[min]=t;
+		
+		
+	}
+	printf("\n");
+	print(a,n);
+	
+	
+	
+}
